9e73141d7df0005d374d4e0d9fd9cb90ab148155
zschoche <github@zschoche.org>
2012-09-17 13:36:31 +0200

    .

--- test_src/main.cpp
+++ test_src/main.cpp
@@ -2,21 +2,13 @@
 
 
 #include <mlog/mlog.hpp>
-#include <mlog/file_logger.hpp>
 
 int main()
 {
-	try
-	{
-		mlog::mlogger.reset(new mlog::file_logger_thread_safe("test.txt", ".", 1024 * 1024));
-		for (int i = 0; i < 100000; i++) 
-		{
-			MLOG_TRACE("write a line to the log.");
-		}
-	}
-	catch(std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	MLOG_TRACE("write a trace line to your log.");
+	MLOG_DEBUG("write a debug line to your log.");
+	MLOG_INFO("write a info line to your log.");
+	MLOG_ERROR("write a error line to your log.");
+	MLOG_FATAL("write a fatal line to your log.");
 	return 0;
 }
