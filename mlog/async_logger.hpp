734a794e6a3a704d7983ba99f1b9468503561334
zschoche <github@zschoche.org>
2013-12-08 15:33:44 +0100

    Added async_logger

--- mlog/async_logger.hpp
+++ mlog/async_logger.hpp
@@ -1,19 +1,38 @@
 #ifndef ___ASYNC_LOGGER_HPP___
 #define ___ASYNC_LOGGER_HPP___
 
-#include "logger.hpp"
-#include <boost/detail/lightweight_mutex.hpp>
+#if defined(BOOST_NO_CXX11_VARIADIC_TEMPLATES)
+#include <boost/preprocessor/repetition.hpp>
+#include <boost/preprocessor/control/if.hpp>
+#endif
 
+#include <boost/detail/lightweight_mutex.hpp>
+#include <boost/config.hpp>
 #include <future>
+#include "logger.hpp"
 
 namespace mlog {
 template <typename T> class async_logger : public T {
       public:
 
+#if !defined(BOOST_NO_CXX11_VARIADIC_TEMPLATES)
+
 	template <typename... Args>
 	async_logger(Args &&... args)
 	    : T(std::forward<Args>(args)...) {}
 
+#else
+
+#define CTOR(z, n, unused)                                                     \
+	BOOST_PP_IF(n, template <, ) BOOST_PP_ENUM_PARAMS(n, typename Arg)     \
+		    BOOST_PP_IF(n, >, )                                        \
+		    async_logger(BOOST_PP_ENUM_BINARY_PARAMS(n, Arg, arg))      \
+	    : T(BOOST_PP_ENUM_PARAMS(n, arg)) {}
+
+	BOOST_PP_REPEAT(5, CTOR, ~)
+#undef CTOR
+
+#endif // ! defined( BOOST_NO_CXX11_VARIADIC_TEMPLATES )
 	virtual ~async_logger() {
 		if (m_result.valid())
 			m_result.wait();
