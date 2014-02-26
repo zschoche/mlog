eea598c2b4668f0ec0caf153be2f75d09a66ac89
zschoche <github@zschoche.org>
2014-02-26 10:40:14 +0100

    Fixed thead-safe

--- mlog/thread_safe.hpp
+++ mlog/thread_safe.hpp
@@ -17,14 +17,14 @@
 
 namespace mlog {
 
-template <class logger_type> class thread_safe : public logger_type {
+template <class logger_type> class thread_safe : public logger<thread_safe<logger_type> > {
       public:
 
 #if !defined(BOOST_NO_CXX11_VARIADIC_TEMPLATES)
 
 	template <typename... Args>
 	thread_safe(Args &&... args)
-	    : logger_type(std::forward<Args>(args)...) {}
+	    : m_logger(std::forward<Args>(args)...) {}
 
 #else
 
@@ -32,7 +32,7 @@ template <class logger_type> class thread_safe : public logger_type {
 	BOOST_PP_IF(n, template <, ) BOOST_PP_ENUM_PARAMS(n, typename Arg)     \
 		    BOOST_PP_IF(n, >, )                                        \
 		    thread_safe(BOOST_PP_ENUM_BINARY_PARAMS(n, Arg, arg))      \
-	    : logger_type(BOOST_PP_ENUM_PARAMS(n, arg)) {}
+	    : m_logger(BOOST_PP_ENUM_PARAMS(n, arg)) {}
 
 	BOOST_PP_REPEAT(5, CTOR, ~)
 #undef CTOR
@@ -44,11 +44,17 @@ template <class logger_type> class thread_safe : public logger_type {
 	template<typename M, typename T>
 	void write_to_log(M&& metadata, T&& log_text) {
 		boost::detail::lightweight_mutex::scoped_lock lock(m_mutex);
-		static_cast<logger_type*>(this)->write_to_log(std::forward<M>(metadata),
+		m_logger.write_to_log(std::forward<M>(metadata),
 					  std::forward<T>(log_text));
 	}
 
+	inline logger_type& get() {
+		return m_logger;
+	}
+	
+
       private:
+	logger_type m_logger;
 	boost::detail::lightweight_mutex m_mutex;
 };
 
